22e6294be499467484c161beacdb057aa43c692a
“guanjian20044@163.com” <guanjian20044>
2020-01-06 14:36:52 +0800

    测试

--- config-client/src/main/java/com/bjdjjs/HelloController.java
+++ config-client/src/main/java/com/bjdjjs/HelloController.java
@@ -1,14 +1,17 @@
 package com.bjdjjs;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 /**
  * Created by djjs on 2020-1-6.
+ * curl -X POST http://localhost:13000/actuator/refresh，返回 ["config.client.version","info.profile"]%
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${info.profile:error}")
     private String profile;
